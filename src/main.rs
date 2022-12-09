62bd1d22227b03a86e58e08b1f247e939814aa23
jatin mahajan <jatinmahajan711@gmail.com>
2022-12-09 18:33:56 -0500

    Updates

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
 
-fn main() {
+fn main(){
     println!("Guess the number game:");
+    loop {
     println!("Please input your Number.");
     let mut guess = String::new();
-    let secret_number = rand::thread_rng().gen_range(1..=2);
+    let secret_number = rand::thread_rng().gen_range(1..=4);
     io::stdin()
         .read_line(&mut guess)
             .expect("Failed to read line");
@@ -23,6 +24,10 @@ fn main() {
         Ordering::Greater => {
             println!("Too big!");
         },
-        Ordering::Equal => println!("You win!")
+        Ordering::Equal => {
+            println!("You win!");
+            break;
+        }
+    }
     }
 }
